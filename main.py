9636b3143cd57a301d4f33bf02a471ca23fc93b6
Matteo <matteorascioni@gmail.com>
2023-03-22 20:18:58 +0100

    adding templates ready to use

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from flask import Flask, render_template
-from post import Post
+from datetime import datetime
 import requests
 
-app = Flask(__name__)
-app.debug = True
-
 posts = requests.get('https://api.npoint.io/603766b691a02027b90a').json()
-post_objects = []
-for post in posts:
-    post_objects.append(Post(post["id"], post["title"], post["subtitle"], post["body"]))
+app = Flask(__name__)
+current_time = datetime.now()
+year = current_time.year
 
 @app.route('/')
-def home():
-    return render_template("index.html", all_posts=post_objects)
+def get_all_posts():
+    return render_template("index.html", all_posts=posts, year=year)
 
-@app.route('/post/<int:route_id>')
-def post_route(route_id):
+@app.route("/post/<int:index>")
+def show_post(index):
     requested_post = None
-    for blog_post in post_objects:
-        if blog_post.id == route_id:
+    for blog_post in posts:
+        if blog_post["id"] == index:
             requested_post = blog_post
-    return render_template("post.html", post=requested_post)
+    return render_template("post.html", post=requested_post, year=year)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", year=year)
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", year=year)
 
 if __name__ == "__main__":
     app.run(debug=True)
